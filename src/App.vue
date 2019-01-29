529ecd9220f6f1f3a444a6c624012b82f3b7b1a2
kotorima <35659278+kotorima@users.noreply.github.com>
2019-01-29 20:14:02 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+        <div class="loader">
+            <div class="point">
+                <div class="one"></div>
+                <div class="two"></div>
+                <div class="three"></div>
+            </div>
+            <p>Loading...</p>
+        </div>
+        <main>
+        </main>
+        <div class="close">
+            <figure class="pop-up">
+            </figure>
+        </div>
+        <ul class="pages">
+        </ul>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+  components:  {
+    'app-header': Header
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+        <a href="../../index.html">
+            <img src="image/Bitmap.png" alt="logo">
+        </a>
+        <ul>
+            <li class="active" id="new"><a href="#">New</a></li>
+            <li id="popular"><a href="#">Popular</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
